e8d340dc2dc3e5c2a8e0df072a7d100dd8598716
Pablo Alpizar <pabloalpizar99@gmail.com>
2021-06-04 00:06:30 -0600

    version 3.2

--- AnalizadorSintactico/src/codigo/frmPrincipal.java
+++ AnalizadorSintactico/src/codigo/frmPrincipal.java
@@ -209,12 +209,12 @@ public class frmPrincipal extends javax.swing.JFrame {
            // Reader lector = new BufferedReader(new FileReader(chooser.getSelectedFile()));
            // Reader file = new BufferedReader(new FileReader(chooser.getSelectedFile()));
             Reader lector;
-            lector = new BufferedReader(new FileReader("C:\\Users\\liset\\Desktop\\Pruebas.c"));
-            Reader file = new BufferedReader(new FileReader("C:\\Users\\liset\\Desktop\\Pruebas.c"));
+            //lector = new BufferedReader(new FileReader("C:\\Users\\liset\\Desktop\\Pruebas.c"));
+            //Reader file = new BufferedReader(new FileReader("C:\\Users\\liset\\Desktop\\Pruebas.c"));
             //lector = new BufferedReader(new FileReader("C:\\Users\\Fabian Martinez\\OneDrive\\Escritorio\\pru.txt"));
             //Reader file = new BufferedReader(new FileReader("C:\\Users\\Fabian Martinez\\OneDrive\\Escritorio\\pru.txt"));
-         //   lector = new BufferedReader(new FileReader("D:\\Desktop\\Pruebas.c"));
-           // Reader file = new BufferedReader(new FileReader("D:\\Desktop\\Pruebas.c"));
+            lector = new BufferedReader(new FileReader("D:\\Desktop\\Pruebas.c"));
+            Reader file = new BufferedReader(new FileReader("D:\\Desktop\\Pruebas.c"));
             LexerCup scanner = new LexerCup(file);
             Lexer lexer = new Lexer(lector);
             Parser parser;
